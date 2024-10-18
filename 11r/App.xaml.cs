4471ec2ba257ae43db54f5100c60f88ff2318426
ku1kova <sofya.kulkova.06@mail.ru>
2024-10-18 08:58:09 +0500

    Update App.xaml.cs

--- 11r/App.xaml.cs
+++ 11r/App.xaml.cs
@@ -1,17 +1,39 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace _11r
 {
     /// <summary>
-    /// Логика взаимодействия для App.xaml
+    /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class MainWindow : Window
     {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void AboutDeveloperMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Разработчик: Кулькова Софья\nКонтакт: sofya.kulkova.06@gmail.com", "О разработчике", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+
     }
 }
